98a311865ca855787cb09abe7cb8fb8e47f59448
Mohamed Irfan <mmohame6@ualberta.ca>
2025-09-09 19:58:52 -0600

    final project

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,24 +1,87 @@
 package com.example.listycity;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+    Button addCity, deleteCity, confirmCity;
+    EditText inputCity;
+    LinearLayout showText;
+    int selectedPosition= -1;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        addCity = findViewById(R.id.add_city);
+        deleteCity = findViewById(R.id.delete_city);
+        confirmCity = findViewById(R.id.confirm_city);
+        inputCity = findViewById(R.id.enter_city);
+        showText = findViewById(R.id.text_input);
+
+        addCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showText.setVisibility(View.VISIBLE);
+            }
+        });
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedPosition = position;
+            }
+        });
+
+        deleteCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedPosition !=-1) {
+                    dataList.remove(selectedPosition);
+                    cityAdapter.notifyDataSetChanged();
+                    selectedPosition = -1;
+                }
+            }
+        });
+
+        confirmCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = inputCity.getText().toString().strip();
+                if (inputText.isEmpty()) {
+                    dataList.add(inputCity.getText().toString());
+                    cityAdapter.notifyDataSetChanged();
+                    inputCity.setText("");
+                    showText.setVisibility(View.GONE);
+                }
+            }
         });
     }
 }

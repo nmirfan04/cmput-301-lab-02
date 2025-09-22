cda0dfc66a0cdcb436ad6e5c8da999636b14d81a
Mohamed Irfan <mmohame6@ualberta.ca>
2025-09-22 13:58:40 -0600

    Add citations

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -46,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         inputCity = findViewById(R.id.enter_city);
         showText = findViewById(R.id.text_input);
 
+        /* The idea of using setVisibility method is from OpenAI, ChatGPT,
+        "i want the list to show up only when an add button in the app is clicked and go away afterwards.
+         how can i do that", 2025-09-09 */
         addCity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +56,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // The idea of using OnItemClickListener is from OpenAI, ChatGPT,
+        // "i tried using setonclicklistener to get an item from a list displayed in the app but i cannot access individual items.
+        //  what can i do to access the item ", 2025-09-09
         cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -70,7 +76,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
+        
+        /* Author: Ahmet Tanakol https://stackoverflow.com/users/1382672/ahmet-tanakol
+        Title: getting a string from a listview item in setOnItemClickListener
+        Date: 2027-07-12
+        License: License: CC-BY-SA 4.0 (International)
+        Date Accessed: 2025-09-09
+        URL: https://stackoverflow.com/questions/11683547/getting-a-string-from-a-listview-item-in-setonitemclicklistener
+        */
         confirmCity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
